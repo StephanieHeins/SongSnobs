d718a187bd9ffd8fba8b6343c2539d1a72b9494f
Troy <troyat88@gmail.com>
2021-11-20 12:47:34 -0800

    completed Login Form(rough) and submit lyrics form

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { PostSong } from './components/PostSong';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
       <>
         <Navbar />
         <Switch>
-          <Route exact path='/' component={SearchBooks} />
+          <Route exact path='/' component={LoginForm} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
